5c23a43e724b039daca183be31b53c2442b4ba31
zoe <emnichtda@gmail.com>
2022-03-23 10:48:17 +0100

    Updated api urls to roleplay.emnichtda.de

--- assets/scripts/chatapp.js
+++ assets/scripts/chatapp.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var token = getCookie("token")
 
     var request = new XMLHttpRequest();
-    request.open("GET", "http://127.0.0.1:3000/" + token, true);
+    request.open("GET", "http://roleplay.emnichtda.de:3000/" + token, true);
     request.setRequestHeader("Content-type", "application/JSON")
 
     request.onreadystatechange = function () {
